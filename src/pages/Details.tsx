cdf15ab6c1227812a05fecdb59079c9292d2a0df
elosis <96085941+elosis@users.noreply.github.com>
2023-08-14 23:32:28 +0300

    revising 2

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,9 +1,24 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+// import { data } from "./Home";
 
 const Details = () => {
   const { id } = useParams();
+  const [getData, setGetData] = useState<any>("");
 
-  return <div>Details</div>;
+  useEffect(() => {
+    if (id !== undefined) {
+      const numberId = Number(id);
+      // setGetData(data.find((item) => item.id === numberId));
+    }
+  }, [id]);
+
+  return (
+    <div>
+      <div>{getData.name}</div>
+      <div>{getData.description}</div>
+    </div>
+  );
 };
 
 export default Details;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
+export interface ProductResponse {
+  id: number;
+  name: string;
+  description: string;
+}
 
 const Home = () => {
   const navigate = useNavigate();
+  const [products, setProducts] = useState<Array<ProductResponse>>([]);
+  const [error, setError] = useState<string>("");
 
-  const data = [
-    {
-      id: 0,
-      name: "React",
-      description: "React exp",
-    },
-    {
-      id: 1,
-      name: "Vue",
-      description: "Vue exp",
-    },
-    {
-      id: 2,
-      name: "Angular",
-      description: "Angular exp",
-    },
-  ];
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get<ProductResponse[]>(
+          "https://dbqqpttscfcjwuygmztw.supabase.co/rest/v1/product?select=*",
+          {
+            headers: {
+              apikey:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicXFwdHRzY2Zjand1eWdtenR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEzNDY5NDQsImV4cCI6MjAwNjkyMjk0NH0.mDo2Q0Oc39rsFS1GVz1PKyBCWZt44AjRKhbdYkk7KXM",
+            },
+          }
+        )
+        .then((res) => setProducts(res.data))
+        .catch((ex) => {
+          const error = ex.response.status === 404 ? "error " : " no error";
+          setError(error);
+        });
+    };
+    getData();
+  }, []);
 
-  return (
-    <div>
-      {data.map((data, index) => (
-        <div
-          onClick={() => navigate("details")}
-          key={index}
-          style={{ cursor: "pointer", marginBottom: "20px" }}
-        >
-          <div>{data.name}</div>
-          <div>{data.description}</div>
-        </div>
-      ))}
-    </div>
-  );
+  console.log(products, "allData");
+
+  return <div>home page</div>;
 };
 
 export default Home;
