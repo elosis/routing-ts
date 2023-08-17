88d1e7c2372a152bdf650cbfd51cd436850bc138
elosis <96085941+elosis@users.noreply.github.com>
2023-08-18 00:41:39 +0300

    setting context

--- src/stores/context.tsx
+++ src/stores/context.tsx
@@ -1,8 +1,72 @@
-import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { createContext, useContext, useState, useEffect } from "react";
 
-const ShopContext = createContext();
+export interface ProductResponse {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
+
+interface ContextValue {
+  products: ProductResponse[];
+  setProducts: React.Dispatch<React.SetStateAction<ProductResponse[]>>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  error: string;
+  setError: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const ShopContext = createContext<ContextValue>({
+  products: [],
+  setProducts: () => {},
+  loading: true,
+  setLoading: () => {},
+  error: "",
+  setError: () => {},
+});
 
 function ShopLayer(props: React.PropsWithChildren<{}>) {
+  const [products, setProducts] = useState<Array<ProductResponse>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    setLoading(true);
+    const getData = async () => {
+      await axios
+        .get(
+          "https://dbqqpttscfcjwuygmztw.supabase.co/rest/v1/product?select=*",
+          {
+            headers: {
+              apikey:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicXFwdHRzY2Zjand1eWdtenR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEzNDY5NDQsImV4cCI6MjAwNjkyMjk0NH0.mDo2Q0Oc39rsFS1GVz1PKyBCWZt44AjRKhbdYkk7KXM",
+            },
+          }
+        )
+        .then((res) => setProducts(res.data))
+        .catch((err) => {
+          const error =
+            err.response.status === 404
+              ? "oh no ! there is an error"
+              : "no error";
+
+          setError(error);
+        });
+    };
+    getData();
+  }, []);
+
+  const data: ContextValue = {
+    products,
+    setProducts,
+    loading,
+    setLoading,
+    error,
+    setError,
+  };
+
   return (
     <ShopContext.Provider value={data}>{props.children} </ShopContext.Provider>
   );
