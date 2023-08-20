9414f8430aca34df91aef9da4e852c4a2da2ce66
elosis <96085941+elosis@users.noreply.github.com>
2023-08-20 12:26:38 +0300

    arraning css for home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,22 +23,55 @@ const Home = () => {
     <div>Loading...</div>
   ) : (
     <div className="container">
-      {products?.map((data, i) => (
-        <div
-          onClick={() => navigate(`details/${data.id}`)}
-          key={i}
-          style={{ marginBottom: "50px", cursor: "pointer" }}
-        >
-          <div>{data?.name}</div>
-          <img
-            src={data?.imageUrl}
-            alt={data?.name}
-            style={{ width: "250px" }}
-          />
-          <div>{data?.description}</div>
-          <div>{data?.price}</div>
+      <div className="main">
+        <div className="title">
+          <h1>Ticketless</h1>
+          <h2 className="subtitle">Gigs you don't want to miss!</h2>
         </div>
-      ))}
+        <div className="gigs-container">
+          {products?.map((data, i) => (
+            <div
+              className="product-container"
+              onClick={() => navigate(`details/${data.id}`)}
+              key={i}
+              style={{ marginBottom: "50px", cursor: "pointer" }}
+            >
+              <div>{data?.name}</div>
+              <img
+                src={data?.imageUrl}
+                alt={data?.name}
+                style={{ width: "250px" }}
+              />
+              <div>{data?.description}</div>
+              <div>{data?.price}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <footer className="footer">
+        <p>
+          <strong>Ticketless</strong>by{" "}
+          <a href="https://serverlesslab.com">Serverlesslab</a>. A sample
+          application for learning Serverless on AWS.
+        </p>
+        <p>
+          The <a href="https://github.com/lucpod/ticketless">source code</a> is
+          licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
+          website content is licensed{" "}
+          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+            CC BY NC SA 4.0
+          </a>
+          .
+        </p>
+        <p>
+          <a href="http://bulma.io" target="_blank">
+            <small>
+              Made with <strong>bulma</strong>
+            </small>
+          </a>
+        </p>
+      </footer>
     </div>
   );
 };
